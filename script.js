6451d0d12e0d77c19b0e29e590b30aa5b1ef426a
JeydenJ <Jeyden.Johnson-1@student.uts.edu.au>
2023-06-09 21:05:56 +1000

    added some css and html

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
-  
+function showSecondCard(event) {
+    event.preventDefault();
+    document.getElementById('stayUpdatedCard').classList.add('hidden');
+    document.getElementById('thankYouCard').classList.remove('hidden');
+    
+}
+function dismissThankYouCard() {
+    document.getElementById('thankYouCard').classList.add('hidden');
+}
