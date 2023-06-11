a828a3513fa715842a2bc6fe82e6f713e739da6d
JeydenJ <Jeyden.Johnson-1@student.uts.edu.au>
2023-06-11 21:07:37 +1000

    added error validation css and js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function showSecondCard(event) {
-    event.preventDefault();
+function showSecondCard() {
     document.getElementById('stayUpdatedCard').classList.add('hidden');
     document.getElementById('thankYouCard').classList.remove('hidden');
     
 }
 function dismissThankYouCard() {
     document.getElementById('thankYouCard').classList.add('hidden');
+}
+function validateEmail(event){
+    event.preventDefault();
+    var emailInput = document.getElementById("userEmail");
+    var email = emailInput.value;
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)){
+        document.getElementById('userEmail').classList.add('error');
+        document.getElementById("email-error").style.display = "block";
+    } 
+    else{
+        document.getElementById('userEmail').classList.remove('error');
+        document.getElementById("email-error").style.display = "none";
+        showSecondCard();
+    }
 }
